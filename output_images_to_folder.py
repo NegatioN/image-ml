002acb075234b892052e807af801a540863a72d1
NegatioN <joakimrishaug@gmail.com>
2016-12-31 15:31:52 +0100

    update script with crop to imagenet

--- output_images_to_folder.py
+++ output_images_to_folder.py
@@ -1,10 +1,32 @@
 from bs4 import BeautifulSoup
 import os
 import shutil
+from PIL import Image
 
 output_dir = "output_ordered"
+path = "."
+filenames = os.listdir(path)
+imagenet_size = 224
+do_crop = True
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
+
+def find_center_image(annotation):
+    bound_box = annotation.object.bndbox
+    xmin = int(bound_box.xmin.text)
+    xmax = int(bound_box.xmax.text)
+    ymin = int(bound_box.ymin.text)
+    ymax = int(bound_box.ymax.text)
+    return (xmax+xmin)/2, (ymax+ymin)/2
+
+
+def crop_to_imagenet(filename, annotation):
+    img = Image.open(filename)
+    center_x, center_y = find_center_image(annotation)
+    half_size = imagenet_size/2
+    return img.crop((center_x-half_size, center_y-half_size,
+                        center_x+half_size, center_y+half_size))
+
 
 def imagenet_to_theano_format(filename):
     with open(filename) as xml_file:
@@ -12,12 +34,14 @@ def imagenet_to_theano_format(filename):
         annotation = soup.html.body.annotation
         image_path = annotation.path.text
         label = annotation.object.find('name').text
+        output_filename = '{}/{}/{}.{}'.format(output_dir, label, annotation.filename.text, "jpg")
         if not os.path.exists('{}/{}'.format(output_dir,label)):
             os.makedirs('{}/{}'.format(output_dir,label))
-        shutil.copyfile(image_path, '{}/{}/{}.{}'.format(output_dir, label, annotation.filename.text, "jpg"))
-
-path = "."
-filenames = os.listdir(path)
+        if not do_crop:
+            shutil.copyfile(image_path, output_filename)
+        else:
+            cropped_img = crop_to_imagenet(image_path, annotation)
+            cropped_img.save(output_filename)
 
 for filename in filenames:
     if ".xml" in filename:
