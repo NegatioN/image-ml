c01f0d8137760c40ed8a8504ec35ebe91af5448c
NegatioN <joakimrishaug@gmail.com>
2017-01-02 02:26:14 +0100

    update script to take in several bound-boxes per pic

--- output_images_to_folder.py
+++ output_images_to_folder.py
@@ -2,17 +2,17 @@ from bs4 import BeautifulSoup
 import os
 import shutil
 from PIL import Image
+import pdb
 
 output_dir = "output_ordered"
-path = "."
+path = "/home/joakim/projects/VideoLabeler/videolabeler/videolabeler/output/Carib-020516-090-FHD"
 filenames = os.listdir(path)
 imagenet_size = 224
 do_crop = True
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-def find_center_image(annotation):
-    bound_box = annotation.object.bndbox
+def find_center_image(bound_box):
     xmin = int(bound_box.xmin.text)
     xmax = int(bound_box.xmax.text)
     ymin = int(bound_box.ymin.text)
@@ -20,9 +20,9 @@ def find_center_image(annotation):
     return (xmax+xmin)/2, (ymax+ymin)/2
 
 
-def crop_to_imagenet(filename, annotation):
+def crop_to_imagenet(filename, labeled_object):
     img = Image.open(filename)
-    center_x, center_y = find_center_image(annotation)
+    center_x, center_y = find_center_image(labeled_object.bndbox)
     half_size = imagenet_size/2
     return img.crop((center_x-half_size, center_y-half_size,
                         center_x+half_size, center_y+half_size))
@@ -34,14 +34,16 @@ def imagenet_to_theano_format(filename):
         annotation = soup.html.body.annotation
         image_path = annotation.path.text
         label = annotation.object.find('name').text
-        output_filename = '{}/{}/{}.{}'.format(output_dir, label, annotation.filename.text, "jpg")
         if not os.path.exists('{}/{}'.format(output_dir,label)):
             os.makedirs('{}/{}'.format(output_dir,label))
         if not do_crop:
+            output_filename = '{}/{}/{}.{}'.format(output_dir, label, annotation.filename.text, "jpg")
             shutil.copyfile(image_path, output_filename)
         else:
-            cropped_img = crop_to_imagenet(image_path, annotation)
-            cropped_img.save(output_filename)
+            for index, labeled_object in enumerate(annotation.find_all('object')):
+                output_filename = '{}/{}/{}-{}.{}'.format(output_dir, label, annotation.filename.text, index, "jpg")
+                cropped_img = crop_to_imagenet(image_path, labeled_object)
+                cropped_img.save(output_filename)
 
 for filename in filenames:
     if ".xml" in filename:
